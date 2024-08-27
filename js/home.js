a38aea8dcbe5f4cb3f46182d5dd8f9ade78c1c88
paulohenrique63 <paulo.golova@gmail.com>
2024-08-27 08:08:17 -0300

    ajustei o h1 da paginal dos graficos, para ele acompanhar o menu

--- js/home.js
+++ js/home.js
@@ -1,8 +1,10 @@
 document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
+    document.querySelector('main h1').classList.toggle('move-title'); // Adiciona a classe para mover o título
 });
 
-document.getElementById("logout_btn").onclick = function() {
+
+document.getElementById("logout_btn").onclick = function () {
     window.location.href = "/index.html";
 };
 
