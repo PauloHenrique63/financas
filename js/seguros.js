5fba42afff58ba89fe008f87c793271b94e65068
paulohenrique63 <paulo.golova@gmail.com>
2024-09-05 23:54:01 -0300

    pagina de seguros feita

---
+++ js/seguros.js
@@ -0,0 +1,9 @@
+document.getElementById('open_btn').addEventListener('click', function () {
+    document.getElementById('sidebar').classList.toggle('open-sidebar');
+    document.querySelector('main h1').classList.toggle('move-title'); // Adiciona a classe para mover o título
+});
+
+
+document.getElementById("logout_btn").onclick = function () {
+    window.location.href = "/index.html";
+};
